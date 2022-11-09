4d8c791db1a57215dd5dd78a679a132b15bb4a69
Islombek Yadgarov <islam.yadgarov.677@gmail.com>
2022-11-09 22:02:24 +0500

    ""

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,23 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        double bb =  sideLength / 2;
+        int aa = (int)bb;
+        for (int i = 0; i < sideLength; i++) {
+            System.out.println();
+            for (int j = 0; j < sideLength; j++) {
+                if (j == aa && i != aa) {
+                    System.out.print('8');
 
+                }
+
+                if (i == aa){
+                    System.out.print('8');
+
+                }else {
+                    System.out.print(' ');
+                }
+            }
+        }
     }
 }
